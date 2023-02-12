1d9873f6b00fbe41c0069c323cf9641df35d1003
Austin Ftacnik <austin@ftacnik.com>
2023-02-11 22:44:07 -0800

    chore(eslint): resolve cannot find module 'next/babel'

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,18 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next/core-web-vitals", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
   },
+    "overrides": [
+    {
+      // Adapt to your needs (e.g. some might want to only override "next.config.js")
+      "files": ["*.js"],
+      // This is the default parser of ESLint
+      "parser": "espree",
+      "parserOptions": {
+        "ecmaVersion": 2020
+      }
+    }
+  ]
 };
