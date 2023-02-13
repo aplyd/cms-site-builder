2679e202fa7417019556f529cc5d54d877db9c78
Austin Ftacnik <austin@ftacnik.com>
2023-02-12 16:23:42 -0800

    chore: remove turbo boilerplate

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -6,9 +6,7 @@ module.exports = {
   },
     "overrides": [
     {
-      // Adapt to your needs (e.g. some might want to only override "next.config.js")
       "files": ["*.js"],
-      // This is the default parser of ESLint
       "parser": "espree",
       "parserOptions": {
         "ecmaVersion": 2020
