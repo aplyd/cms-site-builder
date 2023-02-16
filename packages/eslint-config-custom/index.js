3b4bc38fa7b2bbc431bc89be7e46f11f9d769137
Austin Ftacnik <austin@ftacnik.com>
2023-02-16 14:49:41 -0800

    chore(eslint): add airbnb config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,17 +1,18 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "turbo", "prettier"],
-  plugins: ["prettier"],
+  extends: ['airbnb', 'airbnb/hooks', 'next/core-web-vitals', 'turbo', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
   },
-    "overrides": [
+  overrides: [
     {
-      "files": ["*.js"],
-      "parser": "espree",
-      "parserOptions": {
-        "ecmaVersion": 2020
-      }
-    }
-  ]
+      files: ['*.js'],
+      parser: 'espree',
+      parserOptions: {
+        ecmaVersion: 2020,
+      },
+    },
+  ],
 };
